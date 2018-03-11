eb59f566fd7a26b421c4be3bafd5eadfd5c133f7
Andy D <andyd2k@hotmail.com>
2018-03-11 14:05:46 -0400

    serial version along with some bug fixes

--- shortest.cpp
+++ shortest.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -9,117 +10,133 @@
 using namespace std;
 
 void swap(char *a, char *b);
-void permutations(char *input, int l, int r, int &j, vector<vector<int> > &permute_list);
+void permutations(char *input, int l, int r, int n, int &j, vector<vector<int> > &permute_list);
 int factorial(int n);
-// vector<vector<int> > shortest_list(vector<vector<int> > permute_list, int **cities);
-int check_graph(vector<int> path, int **cities);
+void shortest_list(vector<vector<int> > permute_list, int **cities);
+int check_graph(vector<int> path, int **cities, int min);
 
 int main(int argc, char *argv[]) {
 
-    if(argc < 4) { 
-        printf("Please use the right number of arguments\n");
-        exit(0);
-    }
+	if (argc < 4) {
+		printf("Please use the right number of arguments\n");
+		exit(0);
+	}
 
-    int n = atoi(argv[1]);
-    int threads = atoi(argv[2]);
+	int n = atoi(argv[1]);
+	int threads = atoi(argv[2]);
 
-    printf("n: %d and threads: %d\n", n, threads);
+	printf("n: %d and threads: %d\n", n, threads);
 
-    int **cities = new int*[n];
-    for(int i = 0; i < n; i++)
-        cities[i] = new int[n];
-    
-    string line;
+	int **cities = new int*[n];
+	for (int i = 0; i < n; i++)
+		cities[i] = new int[n];
 
-    ifstream inputFile(argv[3]);
+	string line;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            inputFile >> cities[i][j];
-        }
-    }
+	ifstream inputFile(argv[3]);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << cities[i][j] << " ";
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			inputFile >> cities[i][j];
+		}
+	}
 
-    int fact = factorial(n);
-    vector<vector<int> > permute_list(fact, vector<int>(n - 1, 0));    
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << cities[i][j] << " ";
+		}
+		cout << endl;
+	}
 
-    char *numbers = new char[n];
-    for(int i = 0; i < (n - 1); i++) {
-        numbers[i] = (i + 1) + '0';
-    }
-    numbers += '\0';
+	int fact = factorial(n);
+	vector<vector<int> > permute_list(fact, vector<int>(n - 1, 0));
 
-    int j = 0;
-    permutations(numbers, 0, n - 2, j, permute_list);
+	char *numbers = new char[n];
+	for (int i = 0; i < (n - 1); i++) {
+		numbers[i] = (i + 1) + '0';
+	}
+	numbers[n - 1] = 0;
 
-    printf("fact: %d, size: %d, index: %d\n", fact, static_cast<int>(permute_list.size()), j);
+	int j = 0;
+	permutations(numbers, 0, n - 2, n, j, permute_list);
 
-    static const int arr[] = {0, 1, 2, 3, 4};
-    vector<int> test (arr, arr + sizeof(arr) / sizeof(arr[0]) );
-    printf("%d\n", check_graph(test, cities));
+	printf("fact: %d, size: %d, index: %d\n", fact, static_cast<int>(permute_list.size()), j);
+
+	shortest_list(permute_list, cities);
 }
 
 void swap(char *a, char *b) {
-    char temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	char temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 int factorial(int n) {
-    int value = 1;
-    for(int i = n - 1; i > 1; i--)
-        value *= i;
-    return value;
+	int value = 1;
+	for (int i = n - 1; i > 1; i--)
+		value *= i;
+	return value;
 }
 
-void permutations(char *input, int l, int r, int &j, vector<vector<int> > &permute_list) {
+void permutations(char *input, int l, int r, int n, int &j, vector<vector<int> > &permute_list) {
 
-    int n = sizeof(input)/sizeof(*input);
-
-    if(l == r) {
-        vector<int> temp(n);
-        temp.at(0) = 0;
-        for(int i = 1; i < n; i++)
-            temp.at(i) = input[i];
-        permute_list.at(j) = temp;
-        j++;
-    } else {
-        for(int i = l; i <= r; i++) {
-            swap((input + l), (input + i));
-            permutations(input, l+1, r, j, permute_list);
-            swap((input + l), (input + i));
-        }
-    }
+	if (l == r) {
+		vector<int> temp(n);
+		temp.at(0) = 0;
+		for (int i = 0; i < n - 1; i++)
+			temp.at(i + 1) = input[i] - '0';
+		permute_list.at(j) = temp;
+		j++;
+	}
+	else {
+		for (int i = l; i <= r; i++) {
+			swap((input + l), (input + i));
+			permutations(input, l + 1, r, n, j, permute_list);
+			swap((input + l), (input + i));
+		}
+	}
 };
 
-// vector<vector<int> > shortest_list(vector<vector<int> > permute_list, int **cities) {
 
-//     int min = -1;
+void shortest_list(vector<vector<int> > permute_list, int **cities) {
 
+	vector<int> shortest_path;
+	int cost = INT_MAX;
 
-// }
+	for (size_t i = 0; i < permute_list.size(); i++) {
+		int temp = check_graph(permute_list.at(i), cities, cost);
+		if (cost > temp) {
+			cost = temp;
+			shortest_path = permute_list.at(i);
+		}
+	}
 
-int check_graph(vector<int> path, int **cities) {
+	cout << "Shortest path is:" << endl;
+	for (int x : shortest_path)
+		printf("%d ", x);
+	printf("\ntotal weight = %d\n", cost);
+}
 
-    int path_sum = 0;
-    int path_size = path.size();
-    int i = 0;
-    int j = 1;
+int check_graph(vector<int> path, int **cities, int cost) {
 
-    while(j < path_size) {
-        printf("%d\n", cities[i][j]);
-        path_sum += cities[i][j];      
-        i++;
-        j++;
-    }
+	int path_sum = 0;
+	int path_size = path.size();
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	int y = 1;
 
-    return path_sum;
+	while (y < path_size) {
+		i = path[x];
+		j = path[y];
+		path_sum += cities[i][j];
+		if (path_sum > cost) {
+			break;
+		}
+		x++;
+		y++;
+	}
+
+	return path_sum;
 }
