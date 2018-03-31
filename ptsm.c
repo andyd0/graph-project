3c22c3b76c96fd20f65c7c93c4a8182f857c91bf
Andy D <andyd2k@hotmail.com>
2018-03-31 16:47:44 -0400

    done. including c and cpp

--- ptsm.c
+++ ptsm.c
@@ -1,11 +1,10 @@
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 
-using namespace std;
+// Gets length of array based on type of array
+#define LENGTH(x)  (sizeof(x) / sizeof((x)[0]))
 
 int factorial(int n);
 
@@ -14,26 +13,28 @@ int main(int argc, char *argv[]) {
 	int n = atoi(argv[1]);
 	int threads = atoi(argv[2]);
 
-	int **cities = new int*[n];
+	int **cities = (int **)malloc(n * sizeof(int *));
 	for (int i = 0; i < n; i++)
-		cities[i] = new int[n];
+		cities[i] = (int *)malloc(n * sizeof(int));
 
-	ifstream inputFile(argv[3]);
+	FILE *file;
+	file = fopen(argv[3], "r");
+	if (file == NULL) exit(1);
 
 	// Filling in matrix from input
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			inputFile >> cities[i][j];
-		}
+		for (int j = 0; j < n; j++)
+			fscanf(file, "%d", &cities[i][j]);
 	}
 
 	// Building a path initially in ascending order.  
 	// Will check the path again but cost insignificant
-	std::string initial = "";
+	char *initial = (char *)malloc(n * sizeof(char));
 	for (int i = 1; i < n; i++)
-		initial += i + '0';
+		initial[i - 1] = i + '0';
+	initial[n - 1] = '\0';
 
-	int length = initial.size();
+	int length = strlen(initial);
 	int cost = 0;
 
 	// Gets the cost of the initial path to have something
@@ -44,7 +45,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Will be used to keep track of cost and min path
-	std::string min_path = initial;
+	char * min_path = malloc(strlen(initial) + 1);
+	strcpy(min_path, initial);
 
 	// Number of permutations expected not including 0
 	int fact = factorial(length);
@@ -59,10 +61,13 @@ int main(int argc, char *argv[]) {
 	# pragma omp parallel for		
 	for (int i = 0; i < fact; i++)
 	{
-		std::string avail = initial;
-		std::string path_build = "";
+		char * avail = malloc(strlen(initial) + 1);
+		strcpy(avail, initial);
+		char * path_build = malloc(strlen(initial) + 1);
+		path_build[n - 1] = '\0';
 		int prev = 0;
 		int path_cost = 0;
+		int path_length = 0;
 
 		for (int j = length, divisor = fact; j > 0; j--)
 		{
@@ -78,7 +83,8 @@ int main(int argc, char *argv[]) {
 				break;
 			}
 			else {
-				path_build += avail[index];
+				path_build[path_length] = avail[index];
+				path_length++;
 				prev = avail[index] - '0';
 			}
 
@@ -89,18 +95,22 @@ int main(int argc, char *argv[]) {
 		// length.  If it is, that means we have found a minimum
 		// path.  Since this is done in parallel, path may change
 		// but still a legitimate path.
-		if (path_build.size() == min_path.size()) {
+		if (path_length == length) {
 			min_path = path_build;
 			cost = path_cost;
 		}
 	}
 
-	cout << "\nBest path is: ";
-	cout << "0 ";
-	for (char c : min_path)
-		cout << c << " ";
-	cout << endl;
-	cout << "Distance: " << cost << endl;
+
+	// Prints out results
+	printf("%s", "Best path is: 0 ");
+	for(int i = 0; i < length; i++)
+		printf("%c ", min_path[i]);
+	printf("\nDistance: %d\n", cost);
+
+	free(initial);
+	free(cities);
+	free(min_path);
 }
 
 // Gets the factorial value based on n
