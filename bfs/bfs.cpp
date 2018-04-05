bbff4017c3b5f59cbd8fa97e1961cd3d02d19285
Andy D <andyd2k@hotmail.com>
2018-04-05 16:41:20 -0400

    some changes to files.  still no parallel

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -1,35 +1,128 @@
 #include <iostream>
 #include <limits>
 #include <list>
+#include <omp.h>
 #include <queue>
+#include <vector>
+#include <string>
 #include "graph.h"
 
-int* BFS(Graph G, int V, int r) {
+// Based on the CLRS algorithm
+int* BFS(Graph G, int source) {
 
-	int* parents = new int[V];
+	int num_vertices = G.vertexCount();
+	int* parents = new int[num_vertices];
 	int maxInt = std::numeric_limits<int>::max();
 
-	for (int i = 0; i < V; i++)
+	for (int i = 0; i < num_vertices; i++)
+		parents[i] = maxInt;
+
+	parents[source] = 0;
+	std::queue<int> currentLevel;
+	currentLevel.push(source);
+
+	while (!currentLevel.empty()) {
+		int u = currentLevel.front();
+		currentLevel.pop();
+		for (int v : G.getAdj(u)) {
+			if (parents[v] == maxInt) {
+				parents[v] = u;
+				currentLevel.push(v);
+			}
+		}
+	}
+	return parents;
+}
+
+int* BFSP(Graph G, int r) {
+
+	int num_vertices = G.vertexCount();
+	int* parents = new int[num_vertices];
+	int maxInt = std::numeric_limits<int>::max();
+
+	for (int i = 0; i < num_vertices; i++)
 		parents[i] = maxInt;
 
 	parents[r] = 0;
-	std::queue<int> currentQueue;
-	currentQueue.push(r);
-	std::queue<int> nextQueue;
+	std::queue<int> currentLevel;
+	currentLevel.push(r);
 
-	while (!currentQueue.empty()) {
-		nextQueue = std::queue<int>();
-		while (!currentQueue.empty()) {
-			int u = currentQueue.front();
-			currentQueue.pop();
+	while (!currentLevel.empty()) {
+		int u = currentLevel.front();
+		currentLevel.pop();
+		for (int v : G.getAdj(u)) {
+			if (parents[v] == maxInt) {
+				parents[v] = u;
+				currentLevel.push(v);
+			}
+		}
+	}
+	return parents;
+}
+
+bool verifyBFSTree(Graph G, int source, int *parents) {
+	int num_vertices = G.vertexCount();
+	std::vector<int> depth(num_vertices, -1);
+	depth.at(source) = 0;
+
+	std::queue<int> currentLevel;
+	currentLevel.push(source);
+
+	while (!currentLevel.empty()) {
+		int u = currentLevel.front();
+		currentLevel.pop();
+		for (int v : G.getAdj(u)) {
+			if (depth.at(v) == -1) {
+				depth.at(v) = depth.at(u) + 1;
+				currentLevel.push(v);
+			}
+		}
+	}
+
+	for (int u = 0; u < num_vertices; u++) {
+		if (u == source) {
+			continue;
+		}
+		else {
 			for (int v : G.getAdj(u)) {
-				if (parents[v] == maxInt) {
-					parents[v] = u;
-					nextQueue.push(v);
+				if (parents[v] == u) {
+					if (depth[v] != depth[u] + 1)
+						return false;
+					else
+						continue;
 				}
 			}
 		}
-		currentQueue = nextQueue;
 	}
-	return parents;
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+
+	int V = atoi(argv[1]);
+	int source = atoi(argv[2]);
+	std::ifstream inputFile(argv[3]);
+	
+	int verify = 0;
+
+	if(argc == 5)
+		verify = atoi(argv[4]);
+
+	Graph G(V);
+	G.generate(inputFile);
+
+	int* parents = BFSP(G, source);
+
+	for (int i = 0; i < V; i++)
+		std::cout << parents[i] << " ";
+	std::cout << std::endl;
+
+	bool validTree = false;
+	if (verify == 1)
+		validTree = verifyBFSTree(G, source, parents);
+	std::string test = (validTree) ? "True" : "False";
+
+	std::cout << "Valid tree?: " << test << std::endl;
+
+	delete(parents);
 }
--- bfs/bfs.h
+++
@@ -1,8 +0,0 @@
-#ifndef BFS_H
-#define BFS_H
-
-#include "graph.h"
-
-int* BFS(Graph G, int V, int r);
-
-#endif
